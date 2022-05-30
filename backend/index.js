69c1105df73ae2b59b0647a0ecb4239523f20353
HalaNHasan <hala_nazeeh@yahoo.com>
2022-05-30 22:44:45 +0300

    initialized frontend folder and required packages

--- backend/index.js
+++ backend/index.js
@@ -4,8 +4,10 @@ const { application, json } = require("express");
 const express=require("express");
 const app=express();
 const db=require("./models/db");
+const cors=require("cors");
 
 app.use(express.json());
+app.use(cors());
 const PORT=process.env.PORT;
 
 //require routers
