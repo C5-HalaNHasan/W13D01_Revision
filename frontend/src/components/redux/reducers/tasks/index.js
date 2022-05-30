5bda4099abfd1579cc88afaf6b3cbd57234aa1e2
HalaNHasan <hala_nazeeh@yahoo.com>
2022-05-30 23:14:18 +0300

    created tasks reducer/updated store.js

---
+++ frontend/src/components/redux/reducers/tasks/index.js
@@ -0,0 +1,36 @@
+import { createSlice } from "@reduxjs/toolkit"; 
+
+const tasksSlice=createSlice({
+    name:"tasks",
+    initialState:{
+        tasks:[] //array of tasks objects
+    },
+    reducers:{
+        setTasks:(state,action)=>{
+        // action:{payload:[array of tasks]}
+        state.tasks=action.payload;
+        },
+        addTasks:(state,action)=>{
+        // action:{payload:{object contains task info}}
+        state.tasks.push(action.payload)
+        },
+        updateTaskById:(state,action)=>{
+        // action:{payload:{id,taskName,isCompleted}}
+        state.tasks=state.tasks.map((task)=>{
+            if(task.id == action.payload.id){
+                return {...task,taskName:action.payload.taskName||task.taskName,isCompleted:action.payload.isCompleted||task.isCompleted}
+            }else{
+                return task;
+            }
+        })
+
+        },
+        deleteTaskById:(state,action)=>{
+        // action:{payload:id of the deleted task}
+        state.tasks.splice(action.payload,1)
+        },
+    }
+})
+
+const {setTasks,addTasks,updateTaskById,deleteTaskById}=tasksSlice.actions;
+export default tasksSlice.reducer;
--- frontend/src/components/redux/store.js
+++ frontend/src/components/redux/store.js
@@ -1,7 +1,10 @@
 import {configureStore} from "@reduxjs/toolkit";
+import authReducer from "./reducers/auth/index";
+import tasksReducer from "./reducers/tasks/index"
 
 export default configureStore({
     reducer:{
-
+        auth:authReducer,
+        tasks:tasksReducer,
     }
 });
