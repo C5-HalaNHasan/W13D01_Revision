4b35fcf3b31377254ef5371b270c333c509ecda0
HalaNHasan <hala_nazeeh@yahoo.com>
2022-05-30 22:59:19 +0300

    created auth reducer

---
+++ frontend/src/components/redux/reducers/auth/index.js
@@ -0,0 +1,22 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const authSlice=createSlice({
+    name:"auth",
+    initialState:{
+        token: localStorage.getItem("token") || "",
+    },
+    reducers:{
+        setLogin:(state,action)=>{
+            // action:{payload:token from the login backend response}
+            state.token=action.payload;
+        },
+        setLogout:(state,action)=>{
+            state.token="";
+            localStorage.clear();
+        },
+
+    }
+});
+
+export const {setLogin,setLogout}=authSlice.actions;
+export default authSlice.reducer;
